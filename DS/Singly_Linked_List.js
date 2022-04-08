e1ed3dbbd93c72ef9b8387ca589be72a8d980dd0
jesspesale <jess_pesale@aol.com>
2022-04-08 00:41:08 -0400

    added pop method

--- DS/Singly_Linked_List.js
+++ DS/Singly_Linked_List.js
@@ -1,3 +1,4 @@
+const { timeStamp } = require("console");
 
 class Node{
     constructor(val){
@@ -8,28 +9,45 @@ class Node{
 
 class SinglyLinkedList {
   constructor() {
-      this.head = null;
-      this.tail = null;
-      this.length = 0;
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
-  push(val){
+  push(val) {
     let node = new Node(val);
-    if(this.head === null){
-        this.head = node;
-        this.tail = this.head;
-    }else{
-        this.tail.next = node;
-        this.tail = node;
+    if (this.head === null) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
     }
-    this.length++
-    return this
+    this.length++;
+    return this;
   }
+
+  pop() {
+    if (this.length == 0) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+        newTail = current;
+        current = current.next;
+    }
+    this.tail = newTail
+    this.tail.next = null;
+    this.length--;
+    return newTail.val;
+    }
+
 }
 
-let first = new Node("Hi");
+// let first = new Node("Hi");
 // first.next = new Node("there");
 let list = new SinglyLinkedList
-list.push(first)
+list.push("Hi")
 list.push("there")
+list.push("Jess")
+list.pop()
 console.log(list)
