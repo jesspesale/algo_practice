c8a35a229dd1950a336c17ec3effc1390a1ada55
jesspesale <jess_pesale@aol.com>
2022-04-09 19:42:35 -0400

    added remove method for singly linked list

---
+++ DS/SinglyLinkedList/remove.js
@@ -0,0 +1,63 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  pop() {
+    if (this.length == 0) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return newTail.val;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+    let oldHead = this.head;
+    this.head = this.head.next;
+    this.length--;
+    if (this.length === 0) this.tail = null;
+    return oldHead;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === this.length - 1) return this.pop();
+    if (index === 0) return this.shift();
+    let previous = this.get(index - 1);
+    let removed = previous.next;
+    previous.next = removed.next;
+    this.length--;
+    return removed;
+  }
+}
