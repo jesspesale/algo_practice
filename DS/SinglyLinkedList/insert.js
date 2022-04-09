240271801709576dbc0412af6d04bb159f715a6a
jesspesale <jess_pesale@aol.com>
2022-04-09 18:37:44 -0400

    added the insert method for singly linked list

--- DS/SinglyLinkedList/insert.js
+++ DS/SinglyLinkedList/insert.js
@@ -1,10 +1,8 @@
-const { timeStamp } = require("console");
-
-class Node{
-    constructor(val){
-        this.val = val;
-        this.next = null;
-    }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
 class SinglyLinkedList {
@@ -25,33 +23,6 @@ class SinglyLinkedList {
     }
     this.length++;
     return this;
-  }
-
-  pop() {
-    if (this.length == 0) return undefined;
-    let current = this.head;
-    let newTail = current;
-    while (current.next) {
-      newTail = current;
-      current = current.next;
-    }
-    this.tail = newTail;
-    this.tail.next = null;
-    this.length--;
-    if (this.length === 0) {
-      this.head = null;
-      this.tail = null;
-    }
-    return newTail.val;
-  }
-
-  shift() {
-    if (this.length === 0) return undefined;
-    let oldHead = this.head;
-    this.head = this.head.next;
-    this.length--;
-    if (this.length === 0) this.tail = null;
-    return oldHead;
   }
 
   unshift(val) {
@@ -78,14 +49,6 @@ class SinglyLinkedList {
     return current;
   }
 
-  set(index, value) {
-    let foundNode = this.get(index);
-    if (foundNode === null) return false;
-
-    foundNode.val = value;
-    return true;
-  }
-
   insert(index, value) {
     let newNode = new Node(value);
     if (index < 0 || index > this.length) {
@@ -103,13 +66,4 @@ class SinglyLinkedList {
       return true;
     }
   }
-  
 }
-
-
-let list = new SinglyLinkedList
-list.push("Hi")
-list.push("there")
-list.push("Jess")
-// list.pop()
-// console.log(list)
