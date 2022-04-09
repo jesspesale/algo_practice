8fb441f9ce2dd0452f019a11768af5b5a6730944
jesspesale <jess_pesale@aol.com>
2022-04-09 12:23:18 -0400

    added different files for each individual method on linked list

---
+++ DS/SinglyLinkedList/pop.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  pop() {
+    if (this.length == 0) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return newTail.val;
+  }
+}
