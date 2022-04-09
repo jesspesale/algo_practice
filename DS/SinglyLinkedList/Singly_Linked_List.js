2a60ec6b370cd0bfcd721fbc32dcc244a32d24f2
jesspesale <jess_pesale@aol.com>
2022-04-09 13:16:48 -0400

    added unshift method to add to front of list

--- DS/SinglyLinkedList/Singly_Linked_List.js
+++ DS/SinglyLinkedList/Singly_Linked_List.js
@@ -53,6 +53,19 @@ class SinglyLinkedList {
     if (this.length === 0) this.tail = null;
     return oldHead;
   }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
   
 }
 
