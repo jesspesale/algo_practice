0eb41e1e942d90f5a7669999f09d1dc67a302abf
jesspesale <jess_pesale@aol.com>
2022-04-12 16:08:51 -0400

    added factorizlize function

--- Algorithms/Udemy/Recursion/factorialize.js
+++ Algorithms/Udemy/Recursion/factorialize.js
@@ -5,7 +5,16 @@ function factorialize(num) {
   if (num === 0) return 1;
 
   return num * factorialize(num - 1);
-  // return num;
 }
 
 factorialize(5);
+
+// without recursion
+
+// function factorialize(num){
+//     let total = 1;
+//     for(let i = num; i > 0; i--){
+//         total *= i
+//     }
+//     return total
+// }
